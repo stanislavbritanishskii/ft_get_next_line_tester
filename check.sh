599fb7300aaabacb3057c1d178fee463bf10b7d5
Stanislav Britanishskii <sbritani@2-H-4.42heilbronn.de>
2022-10-28 23:20:18 +0200

    some changes

--- check.sh
+++ check.sh
@@ -7,6 +7,8 @@ second_flag=$2
 SRCS="get_next_line.c get_next_line.h get_next_line_utils.c"
 CFLAGS="-Werror -Wextra -Wall"
 
+LOCALPATH=../get_next_line
+
 norm () {
 	VAR=$1
 	if [ -z $VAR ]
@@ -35,12 +37,48 @@ compile () {
 	rm $RES temp.o
 }
 
-if [ $flag == n ] || [ $flag == "norm" ]
+in_list () {
+	LIST=$1
+	TOFIND=$2
+	for name in $(echo $LIST | tr '\n' ' ')
+	do
+		if [ $name = $TOFIND ]
+		then
+			return 0
+			echo $TOFIND
+		fi
+	done
+	return 1
+}
+
+check_repo () {
+	for name in $SRCS
+	do
+		if ! in_list "$(ls $LOCALPATH)" $name
+		then
+			return 1
+		fi
+	done
+	return 0
+}
+
+if [ -z $flag ]
+
+then
+	echo "no params"
+	if ! check_repo
+	then
+		echo "bad repo"
+	fi
+
+
+
+elif [ $flag == n ] || [ $flag == "norm" ]
 then
 	norm $2
-fi
 
-if [ $flag == c ] || [ $flag == "compile" ]
+
+elif [ $flag == c ] || [ $flag == "compile" ]
 then
 	compile $2 $3
 fi
